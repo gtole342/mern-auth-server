2ada83f11e94ffe1a20c31e6b22fd2390f4573f3
gtole342 <gtole342@gmail.com>
2019-09-11 14:24:35 -0700

    Added user model

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const cors = require('cors')
+const morgan = require('morgan')
 const app = express()
 
 app.use(cors())
+app.use(morgan('dev'))
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json({ limit: '10mb' }))
 
